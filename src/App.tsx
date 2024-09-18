4a084bfba1b1bc0017abeba564ffafa3563ba43e
Pravin Ray <pravin@Pravins-MacBook-Pro.local>
2024-09-19 00:44:39 +0545

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App p-4">
+      Hello, World!
+    </div>
+  );
+};
+
+export default App;
