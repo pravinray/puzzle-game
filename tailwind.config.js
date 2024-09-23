71f52d858f01caf1652f17f0d9cef65b1463e882
Pravin Ray <pravin@Pravins-MacBook-Pro.local>
2024-09-23 12:54:47 +0545

    styles added to blink seconds

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blink: "blink 1s step-start infinite",
+      },
+      keyframes: {
+        blink: {
+          "0%, 100%": { opacity: 1 },
+          "50%": { opacity: 0 },
+        },
+      },
+    },
   },
   plugins: [],
 };
