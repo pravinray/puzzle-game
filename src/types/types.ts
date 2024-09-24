69e4c1d04041cb3d43b237d5564772ae2bef9893
Pravin Ray <pravin@Pravins-MacBook-Pro.local>
2024-09-24 15:32:49 +0545

    types added for multiple data types to handle code more readable and error free

---
+++ src/types/types.ts
@@ -0,0 +1,37 @@
+import { ReactNode } from "react";
+
+type GameContextType = {
+  isSolved: boolean;
+  setIsSolved: (status: boolean) => void;
+};
+
+type GameProviderProps = {
+  children: ReactNode;
+};
+
+type ScoreLevel =
+  | "Excellent"
+  | "Good Job"
+  | "You Can Do Better"
+  | "Please Try Again";
+
+type GameState = {
+  level: number;
+  incorrectMoves: number;
+  timeLeft: number;
+  scoreLevel: ScoreLevel | null;
+  failuresInARow: number;
+};
+
+type ScoreProps = {
+  controlIncorrectMove: number;
+  randomImageAndShufflePositions: () => void;
+};
+
+export type {
+  GameContextType,
+  GameProviderProps,
+  ScoreLevel,
+  GameState,
+  ScoreProps,
+};
